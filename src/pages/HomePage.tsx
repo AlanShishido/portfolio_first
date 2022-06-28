8e7c6ed445cfdf7efaa86ae2e331185f825d68d3
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2022-06-27 22:17:29 -0400

    preparing code

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export function HomePage(){
+  return (
+    <div className="flex">
+      <h1 className="text-green-100">Home Page</h1>
+      <h1>Home Page</h1>
+      <h1>Home Page</h1>
+    </div>
+    )
+}
