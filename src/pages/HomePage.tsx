374482f34ce2cce12cb92ccedfcb2d4fa25275b7
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2022-06-28 22:49:32 -0400

    creating footer and main content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,24 @@
+import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
 
 export function HomePage(){
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+      <div>
+        <div>
+          <span>Welcome to my site</span>
+          <h1>Hi, I'm Alan, a Eletric Enginner </h1>
+
+          <p>Innovation and constant learning. Bachelor of Electrical Engineering.</p>
+        </div>
+
+        <div>
+          <img src="https://github.com/AlanShishido.png" alt="Avatar" />
+        </div>
+
+      </div>
+      <Footer />
     </div>
   )
 }
